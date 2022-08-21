004310e08a3e9e5296be2b906719f86ebccc02a8
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-21 19:08:15 +0900

    Add files via upload

---
+++ join_action.php
@@ -0,0 +1,32 @@
+<?php
+    $connect = mysqli_connect('20.249.82.171','root','password','DB_BOARD') or die("fail");
+
+    $id=$_POST['id'];
+    $pw=$_POST['pw'];
+    $email=$_POST['email'];
+    $name=$_POST['name'];
+
+    $date = date('Y-m-d H:i:s');
+
+    $query = "insert into member (id, pw, date) values ('$id', '$pw', '$date', 0)";
+
+
+    $result = $connect->query($query);
+
+    if ($result) {
+    ?>       <script>
+              alert('가입 되었습니다.');
+              location.replace("./index.php");
+              </script>
+   <?php }
+            else{
+?>              <script>
+
+                        alert("fail");
+                </script>
+<?php    }
+
+                mysqli_close($connect);
+?>
+      
+    
