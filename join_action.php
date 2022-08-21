37ddaa375484cd60a7404d0d7a9e0e81858b2395
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-21 20:23:54 +0900

    Update join_action.php

--- join_action.php
+++ join_action.php
@@ -1,5 +1,5 @@
 <?php
-    $connect = mysqli_connect('20.249.82.171','Team-H','dnjswndbf3.14','DB_BOARD',1853) or die("fail");
+    $connect = mysqli_connect('20.249.82.171','Team-H','dnjswndbf3.14','DB_BOARD') or die("fail");
 
     $id=$_POST['id'];
     $pw=$_POST['pw'];
