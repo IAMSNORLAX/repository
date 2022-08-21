ab5e021dced268414443a94bc212365bdb155142
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-22 00:10:04 +0900

    Add files via upload

---
+++ view.php
@@ -0,0 +1,30 @@
+<?php 
+    $connect = mysqli_connect('localhost,')
+    $number = $_GET['number'];
+    session_start();
+    $query = ""
+    $result = $connect->query($query);
+    $rows - mysqli_fetch_assoc($result);
+?>
+
+<table class="view_table" align=center>
+<tr>
+    <td colspan="4" class="view_title"><?php echo $rows['title']?></td>
+</tr>
+<tr>
+    <td class="view_id">WRITER</td>
+    <td class="view_id"><?php echo $rows['id']?></td>
+    <td class="view_hit">VIEWS</td>
+    <td class="view_hit2"<?php echo $rows['hit']?></td>
+</tr>
+
+<tr>
+    <td colspan="4" class="view_content" vallign="top">
+    <?php echo $rows['content']?></td>
+</tr>
+</table>
+
+<div class="view_btn">
+        <buttion class="view_btn1" onclick="location.href='index.html'">TO BOARD</button>
+        <buttion class="view_btn1" onclick="location.href='modify.php?number=<?=number?>&id=<?$_SESSION['userid']?>'">EDIT</button>
+        <buttion class="view_btn1" onclick="location.href='delete.php?number=<?=number?>&id=<?$_SESSION['userid']?>'">DELETE</button>
