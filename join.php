60666e22b31e4b4c7672863f9a564c1cb5146896
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-21 21:55:43 +0900

    Update join.php

--- join.php
+++ join.php
@@ -8,8 +8,8 @@
         <div align='center'>
                 <p>S I G N U P</p>
                 <form method='post' action='join_action.php'>
-                        <p>ID: <input type="text" name="id"></p>
-                        <p>PW: <input type="password" name="pw"></p>
+                        <p>I D: <input type="text" name="id"></p>
+                        <p>P W: <input type="password" name="pw"></p>
                         <input type="submit" value="J O I N">
                 </form>
             </div>
