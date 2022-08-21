029f07ca92cfb99fc21776870d51dc5e9ea9e329
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-21 20:56:40 +0900

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset = 'utf-8'>
+</head>
+<style>
+    
+</style>
+<body>
+<?php
+
+?>
+
+<h2 align=center> B O A R D</h2>
+<table align = center>
+<tr>
+<td width = "50" align="center">번호</td>
+<td width = "500" align="center">제목</td>
+<td width = "100" align="center">작성자</td>
+<td width = "200" align="center">날짜</td>
+<td width = "50" align="center">조회수</td>
+</tr>
+</thead>
+
+<tbody>
+
+    <td width = "50" align = "center"><?php echo $ total?></td>
+    <td width = "500" align = "center">
+     <a href = "view.php?number=<?php echo $rows['number']?>">
+     <?php echo $rows['title']?></td>
+     <td width = "100" align = "center"><?php echo $rows['id']?></td>
+    <td width = "200" align = "center"><?php echo $rows['date']?></td>
+    <td width = "50" align = "center"><?php echo $rows['hit']?></td>
+    
+</tbody>
+</table>
+
+<div class = text>
+    <font style="cursor: hand"onclick="location.href='write.php'">글 쓰 기</font>
+</body>
+</html>
