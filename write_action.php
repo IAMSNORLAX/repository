11d7e2b5d01563dd628c78186437f2a6b13883bc
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-21 21:51:15 +0900

    Add files via upload

---
+++ write_action.php
@@ -0,0 +1,27 @@
+<?php
+        $connect = mysqli_connect() or die("fail");
+
+        $id = $_GET[name];
+        $pw = $_GET[pw];
+        $title = $GET[title];
+        $content = $_GET[content];
+        $date = date(Y-m-d H:i:s);
+
+        $URL = 'index.html';
+
+        $query = "insert into board (number, title, title content, date, hit, id, pw)
+                valuse(null, '$title', '$content', '$date', 0, '$id', '$pw')";
+
+    $result = $connect->query($query);
+    if($result){
+?>       <script>
+            alert("<?php echo" 글이 등록되었습니다."?>");
+            location.replace("<?php echo $URL?>");
+        </script>
+<?php
+    }
+    else{
+        echo "F A I L";
+    }
+    mysqli_close($connect);
+?>
