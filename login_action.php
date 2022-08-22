2a6398085e1503f97acc153f6c1b31cf9b2ad5cc
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-22 22:46:23 +0900

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -14,7 +14,7 @@
             $row=mysqli_fetch_assoc($result);
 
 
-            if($row['pw']==$pw){
+            if($row['pw']==$pw)
                 $_SESSION['userid']=$id;
                 if(isset($_SESSION['userid'])){
                    echo  "<script> alert('로그인 되었습니다.');
