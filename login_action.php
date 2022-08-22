992b9da87a91a85e45781318b5d3a4f6c994a8f9
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-22 22:53:10 +0900

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -14,7 +14,7 @@
             $row=mysqli_fetch_assoc($result);
 
 
-            if($row['pw']==$pw)
+            if($row['pw']==$pw){
                 $_SESSION['userid']=$id;
                 if(isset($_SESSION['userid'])){
                    echo  "<script> alert('로그인 되었습니다.');
@@ -25,9 +25,10 @@
                 else{
                     echo "session fail";
                 }
+            }
             
             
-            {
+            else{
                 echo  "<script>
                         alert('아이디 혹은 비밀번호가 잘못되었습니다.');
                         history.back();
