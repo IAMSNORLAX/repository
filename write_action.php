99c9aaea214229df85d71d7de43991ea1d0770fa
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-23 17:54:24 +0900

    Update write_action.php

--- write_action.php
+++ write_action.php
@@ -1,23 +1,24 @@
 <?php
-        $connect = mysqli_connect('localhost','team-h','Dnjswndbf3.14','DB_BOARD') or die("fail");
+        $connect = mysqli_connect() or die("fail");
 
         $id = $_GET[name];
         $pw = $_GET[pw];
         $title = $GET[title];
         $content = $_GET[content];
+        $date = date(Y-m-d H:i:s);
 
         $URL = 'index.html';
 
         $query = "insert into board (number, title, title content, date, hit, id, pw)
-                valuse(null, '$title', '$content', '$date', now(), '$id', '$pw')";
+                valuse(null, '$title', '$content', '$date', 0, '$id', '$pw')";
 
     $result = $connect->query($query);
     if($result){
-    echo"<script>
-            alert(' 글이 등록되었습니다.');
-            location.replace('echo $URL?');
-        </script>";
-
+?>       <script>
+            alert("<?php echo" 글이 등록되었습니다."?>");
+            location.replace("<?php echo $URL?>");
+        </script>
+<?php
     }
     else{
         echo "F A I L";
