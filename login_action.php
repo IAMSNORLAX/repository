ab0d117705f3564acbfd2a9c57d338007916486d
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-25 21:56:07 +0900

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -18,7 +18,7 @@
                 $_SESSION['userid']=$id;
                 if(isset($_SESSION['userid'])){
                    echo  "<script> alert('로그인 되었습니다.');
-                        location.replace('index.html');
+                        location.replace('board.html');
                     </script>";
                     
                 }
