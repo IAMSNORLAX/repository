e381d0499f1b2b5ac79e679daa90227d4bd8b7cd
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-26 01:11:19 +0900

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -1,81 +1,46 @@
-<!DOCTYPE html>
-
-<html>
-<head>
-    <meta charset = 'utf-8'>
-</head>
-<style>
-    table{
-        border-top: 1px
-        border-collapse: collapse;
-    }
-    tr{
-        border-bottom: 1px
-        padding: 20px;
-        
-    }
-    td{
-        border-bottom: 1px
-        padding: 20px;
-
-    }
-    table .even{
-        background: #f7f0f0;
-    }
-    .text{
-        text-align:center;
-        padding-top:20px;
-
-    }
-    .text:hover{
-        text-decoration: underline;
-    }
-    
-</style>
-<body>
-    <table align = center>
-    <div class =text>
-    <text-align: right>
-   
-</div>
-
-<h2 align=center> B O A R D</h2>
-    <tr>
-<td width = "50" align="center">번호</td>
-<td width = "500" align="center">제목</td>
-<td width = "100" align="center">작성자</td>
-<td width = "200" align="center">날짜</td>
-<td width = "50" align="center">조회수</td>
-</tr>
-</thead>
-
-<tbody>
 <?php
- $connect = mysqli_connect('localhost', 'team-h', 'Dnjswndbf3.14', 'DB_BOARD') or die ("C O N N E C T  F A I L")
- $query = "select * from board order by number desc";
- $result = $connect->query($query);
- $total = mysqli_num_rows($result);
+        session_start();
 
-            while ($rows = mysqli_fetch_assoc($result)) {
-            
-                    
-                    <td width="50" align="center"> echo $total </td>
-                    <td width="500" align="center">
-                        <a href="view.php?number= echo $rows['number']">
-                             echo $rows['title'] 
-                    </td>
-                    <td width="100" align="center"> echo $rows['id']</td>
-                    <td width="200" align="center"> echo $rows['date']</td>
-                    <td width="50" align="center"> echo $rows['hit']</td>
-                    </tr>
-                
-                $total--;
+        $connect = mysqli_connect('localhost','team-h','Dnjswndbf3.14','DB_BOARD') or die("fail");
+
+        $id=$_GET['id'];
+        $pw=$_GET['pw'];
+
+
+        $query = "select * from member where id='$id'";
+        $result = $connect->query($query);
+
+        if(mysqli_num_rows($result)==1) 
+            $row=mysqli_fetch_assoc($result);
+
+
+            if($row['pw']==$pw){
+                $_SESSION['userid']=$id;
+                if(isset($_SESSION['userid'])){
+                ?>    <script>
+                        alert("로그인 되었습니다.");
+                        location.replace("./login_success");
+                    </script>
+<?php                    
+                }
+                else{
+                    echo "session fail";
+                }
             }
- ?>
-</tbody>
-</table>
-
-<div class = text>
-    <font style="cursor: hand"onclick="location.href='write.php'">W R I T E</font>
-</body>
-</html>
+            
+            else{
+         ?>           <script>
+                        alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+                        history.back();
+                    </script>
+            }
+        <?php
+        }
+            else{
+?>                <script>
+                    alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+                    history.back();
+                    </script>
+<?php
+            }
+?>
