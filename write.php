5aa13aa6cdb23969e408d4fbb83b7567da112a48
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-26 23:21:29 +0900

    Update write.php

--- write.php
+++ write.php
@@ -77,7 +77,7 @@
                     </center>
                     <form enctype='multipart/form-data' action='upload_ok.php' method='post'>
 	<input type='file' name='myfile'>
-	<button>보내기</button>
+	
 </form>
                 </td>
             </tr>
