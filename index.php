dad4535888a625b320319a77ce680063b3894a62
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-26 20:13:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -100,11 +100,6 @@
     
       <div id="search_box">
     <form action="/page/board/search_result.php" method="get">
-      <select name="catgo">
-        <option value="title">제목</option>
-        <option value="name">글쓴이</option>
-        <option value="content">내용</option>
-      </select>
       <input type="text" name="search" size="40" required="required" /> <button>검색</button>
     </form>
     </div>
