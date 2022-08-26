bc93587d2cee1b364da298e65b20f9e92f24948b
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-26 21:13:47 +0900

    Update test.php

--- test.php
+++ test.php
@@ -3,9 +3,9 @@
 $connect = mysqli_connect('localhost','team-h','Dnjswndbf3.14', 'DB_BOARD');
 
 $sql="select * from board order by no desc";
-$result-mysqli_query($con,$sql);
+$result=mysqli_query($con,$sql);
 
-while($row-mysqli_fetch_array($result,MYSQLI_ASSOC)){?>
+while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){?>
 
     <tr>
     <td><?=$row['name']?></td>
