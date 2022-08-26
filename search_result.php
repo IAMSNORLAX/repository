12dc958c66408d30491c7518c52fa9a5f5c7a8bd
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-27 01:51:04 +0900

    Update search_result.php

--- search_result.php
+++ search_result.php
@@ -92,7 +92,7 @@
                     <td width="50" align="center"><?php echo $total ?></td>
                     <td width="500" align="center">
                         <a href="read.php?number=<?php echo $rows['number'] ?>">
-                            <?php echo "%query" ?>
+                            <?php echo "%query2" ?>
                     </td>
                     <td width="100" align="center"><?php echo $rows['id'] ?></td>
                     <td width="200" align="center"><?php echo $rows['date'] ?></td>
