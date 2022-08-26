eaf28085f7bae30ae81b2731f43e4c0428d232da
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-26 16:10:33 +0900

    Update write_action.php

--- write_action.php
+++ write_action.php
@@ -1,28 +1,28 @@
 <?php
-        $connect = mysqli_connect('localhost','team-h','Dnjswndbf3.14','DB_BOARD') or die("fail");
+$connect = mysqli_connect("localhost", "team-h", "Dnjswndbf3.14", "DB_BOARD") or die("fail");
 
-        $id = $_POST["name"];
-        $pw = $_POST["pw"];
-        $title = $_POST["title"];
-        $content = $_POST["content"];
-        $date = date('Y-m-d H:i:s');
+$id = $_POST['name'];                 
+$title = $_POST['title'];               
+$content = $_POST['content'];           
+$date = date('Y-m-d H:i:s');            
 
-     
+$URL = './index.php';                   
 
-        $query = "insert into board (title, content,id, pw, date, hit)
-                values('$title', '$content','$id', '$pw', '$date', 0)";
 
-    $result = $connect->query($query);
-    if($result){
-    echo"<script>
-            alert(' 글이 등록되었습니다.');
-            location.replace('index.php');
-        </script>";
+$query = "INSERT INTO board (number, title, content, date, hit, id) 
+        values(null,'$title', '$content', '$date', 0, '$id')";
 
-    }
-    //else{
-       // echo "F A I L";
-   // }
-    mysqli_close($connect);
+
+$result = $connect->query($query);
+if ($result) {
+?> <script>
+        alert("<?php echo "게시글이 등록되었습니다." ?>");
+        location.replace("<?php echo $URL ?>");
+    </script>
+<?php
+} else {
+    echo "게시글 등록에 실패하였습니다.";
+}
+
+mysqli_close($connect);
 ?>
-
