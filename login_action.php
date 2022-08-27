1f43915038f927cde2d75b91b7de1a9a56743995
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-27 20:35:35 +0900

    Update login_action.php

--- login_action.php
+++ login_action.php
@@ -14,11 +14,7 @@
             $row=mysqli_fetch_assoc($result);
 
 
-        if ( ($id=='') || ($pw=='') ) {  echo "<script>alert('아이디 또는 패스워드를 입력하여 주세요.');
-        history.back();</script>";  
-            exit;
-                                 }
-
+      
         
 
 
