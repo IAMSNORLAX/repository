de237dc75ceb2d0ecc780803577455e2b7905457
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-27 15:03:07 +0900

    Update write_action.php

--- write_action.php
+++ write_action.php
@@ -9,11 +9,30 @@ $date = date('Y-m-d H:i:s');
 $URL = './index.php';                   
 
 
-$query = "INSERT INTO board (number, title, content, date, hit, id) 
-        values(null,'$title', '$content', '$date', 0, '$id')";
+$query = "INSERT INTO board (number, title, content, date, hit, id,file) 
+        values(null,'$title', '$content', '$date', 0, '$id','$name')";
 
 
 $result = $connect->query($query);
+
+
+if($_FILES['upload_file'] != NULL){
+    $tmp_name = $_FILES['upload_file']['tmp_name'];
+    $name = $_FILES['upload_file']['name'];
+    $path = "./files/$username";
+    if(!file_exists($path)){
+        mkdir($path, 0777, true);
+        chmod($path, 0777);
+        $up = move_uploaded_file($tmp_name, "$path/$name");
+    }else{
+        $up = move_uploaded_file($tmp_name, "$path/$name");
+    }
+}else{
+    $name = "NULL";
+}
+
+ 
+
 if ($result) {
 ?> <script>
         alert("<?php echo "게시글이 등록되었습니다." ?>");
@@ -26,3 +45,4 @@ if ($result) {
 
 mysqli_close($connect);
 ?>
+
