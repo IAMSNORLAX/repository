a528122c3d6947dc003e52d26fa9143bcd0369bc
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-27 20:38:15 +0900

    Update read.php

--- read.php
+++ read.php
@@ -108,42 +108,33 @@
     $connect->query($hit);
 
     if (isset($_SESSION['userid'])) {
-    ?><b>
-        <?php echo $_SESSION['userid']; ?>
-    </b>님 반갑습니다.
-    <button onclick="location.href='./logout.php'" style="float:right; font-size:15.5px;">로그아웃</button>
-    <br />
+    ?><b><?php echo $_SESSION['userid']; ?></b>님 반갑습니다.
+        <button onclick="location.href='./logout.php'" style="float:right; font-size:15.5px;">로그아웃</button>
+        <br />
     <?php
     } else {
     ?>
-    <button onclick="location.href='./login.php'" style="float:right; font-size:15.5px;">로그인</button>
-    <br />
+        <button onclick="location.href='./login.php'" style="float:right; font-size:15.5px;">로그인</button>
+        <br />
     <?php
     }
     ?>
 
     <table class="read_table" align=center>
         <tr>
-            <td colspan="4" class="read_title">
-                <?php echo $rows['title'] ?>
-            </td>
+            <td colspan="4" class="read_title"><?php echo $rows['title'] ?></td>
         </tr>
         <tr>
             <td class="read_id">작성자</td>
-            <td class="read_id2">
-                <?php echo $rows['id'] ?>
-            </td>
+            <td class="read_id2"><?php echo $rows['id'] ?></td>
             <td class="read_hit">조회수</td>
-            <td class="read_hit2">
-                <?php echo $rows['hit'] + 1 ?>
-            </td>
+            <td class="read_hit2"><?php echo $rows['hit'] + 1 ?></td>
         </tr>
 
 
         <tr>
             <td colspan="4" class="read_content" valign="top">
-                <?php echo $rows['content'] ?>
-            </td>
+                <?php echo $rows['content'] ?></td>
         </tr>
     </table>
 
@@ -151,86 +142,45 @@
         <button class="read_btn1" onclick="location.href='./index.php'">목록</button>&nbsp;&nbsp;
         <?php
         if (isset($_SESSION['userid']) and $_SESSION['userid'] == $rows['id']) { ?>
-        <button class="read_btn1" onclick="location.href='./modify.php?number=<?= $number ?>'">수정</button>&nbsp;&nbsp;
-        <button class="read_btn1" a onclick="ask();">삭제</button>
-    </div>
-        <!--- 댓글 불러오기 -->
-        <div class="reply_view">
-            <h3>댓글목록</h3>
-            <?php
-$sql3 = query("select * from reply where con_num='".$bno."' order by idx desc");
-while($reply = $sql3->fetch_array()){
-?>
-            <div class="dap_lo">
-                <div><b>
-                        <?php echo $reply['name'];?>
-                    </b></div>
-                <div class="dap_to comt_edit">
-                    <?php echo nl2br("$reply[content]"); ?>
-                </div>
-                <div class="rep_me dap_to">
-                    <?php echo $reply['date']; ?>
-                </div>
-                <div class="rep_me rep_menu">
-                    <a class="dat_edit_bt" href="#">수정</a>
-                    <a class="dat_delete_bt" href="#">삭제</a>
-                </div>
-                <!-- 댓글 수정 폼 dialog -->
-                <div class="dat_edit">
-                    <form method="post" action="/BBS/rep_modify_ok.php">
-                        <input type="hidden" name="rno" value="<?php echo $reply['idx']; ?>" /><input type="hidden"
-                            name="b_no" value="<?php echo $bno; ?>">
-                        <input type="password" name="pw" class="dap_sm" placeholder="비밀번호" />
-                        <textarea name="content" class="dap_edit_t"><?php echo $reply['content']; ?></textarea>
-                        <input type="submit" value="수정하기" class="re_mo_bt">
-                    </form>
-                </div>
-                <!-- 댓글 삭제 비밀번호 확인 -->
-                <div class='dat_delete'>
-                    <form action="/BBS/reply_delete.php" method="post">
-                        <input type="hidden" name="rno" value="<?php echo $reply['idx']; ?>" /><input type="hidden"
-                            name="b_no" value="<?php echo $bno; ?>">
-                        <p>비밀번호<input type="password" name="pw" /> <input type="submit" value="확인"></p>
-                    </form>
-                </div>
-            </div>
-            <?php } ?>
+            <button class="read_btn1" onclick="location.href='./modify.php?number=<?= $number ?>'">수정</button>&nbsp;&nbsp;
+            <button class="read_btn1" a onclick="ask();">삭제</button>
 
-            <!--- 댓글 입력 폼 -->
-            <div class="dap_ins">
-                <form action="/BBS/reply_ok.php?idx=<?php echo $bno; ?>" method="post">
-                    <input type="text" name="dat_user" id="dat_user" class="dat_user" size="15" placeholder="아이디">
-                    <input type="password" name="dat_pw" id="dat_pw" class="dat_pw" size="15" placeholder="비밀번호">
-                    <div style="margin-top:10px; ">
-                        <textarea name="content" class="reply_content" id="re_content"></textarea>
-                        <button id="rep_bt" class="re_bt">댓글</button>
-                    </div>
-                </form>
-            
-        </div>
-        <!--- 댓글 불러오기 끝 -->
-        <div id="foot_box"></div>
-   
+	   
+	    
+           <div class="dap_ins">
+		<form action="reply_ok.php?idx=<?php echo $bno; ?>" method="post">
+			<input type="text" name="dat_user" id="dat_user" class="dat_user" size="15" placeholder="아이디">
+			<input type="password" name="dat_pw" id="dat_pw" class="dat_pw" size="15" placeholder="비밀번호">
+			<div style="margin-top:10px; ">
+				<textarea name="content" class="reply_content" id="re_content" ></textarea>
+				<button id="rep_bt" class="re_bt">댓글</button>
+			</div>
+			
+		</form>
+	</div>
 
 
-    <div>
-        파일 : <a href="./upload/<?php echo $rows['file'];?>" download>
-            <?php echo $rows['file']; ?>
-        </a>
-    </div>
+	    <div>
+파일 : <a href="./upload/<?php echo $rows['file'];?>" download><?php echo $rows['file']; ?></a>
+</div>
 
+	    
 
-    <script>
-        function ask() {
-            if (confirm("게시글을 삭제하시겠습니까?")) {
-                window.location = "./delete.php?number=<?= $number ?>"
-            }
-        }
-    </script>
+</form>
 
-    <?php } ?>
+            <script>
+                function ask() {
+                    if (confirm("게시글을 삭제하시겠습니까?")) {
+                        window.location = "./delete.php?number=<?= $number ?>"
+                    }
+                }
+            </script>
+           
+        <?php } ?>
 
     </div>
 </body>
 
 </html>
+
+
