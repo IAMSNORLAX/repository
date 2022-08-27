a8c9525de189ebfdad41dce6c4bb6eb30b50409b
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-27 19:14:48 +0900

    Create reply_action.php

---
+++ reply_action.php
@@ -0,0 +1,15 @@
+<?php
+ $connect = mysqli_connect('localhost', 'team-h', 'Dnjswndbf3.14', 'DB_BOARD');
+     $bno = $_GET['number'];  
+     session_start();
+   
+    
+    if($bno && $_POST['dat_user'] && $userpw && $_POST['content']){
+        $sql = query("insert into comment(number,id,content, date ,pw) values('".$bno."','".$_POST['dat_user']."','".$_POST['content']."',now() '".$userpw."')");
+        echo "<script>alert('댓글이 작성되었습니다.'); 
+        location.href='/read.php?idx=$bno';</script>";
+    }else{
+        echo "<script>alert('댓글 작성에 실패했습니다.'); 
+        history.back();</script>";
+    }
+?>
