45d6eb65f8bf25ce19a5794fe7f51905a47441ea
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-27 19:15:41 +0900

    Update reply_action.php

--- reply_action.php
+++ reply_action.php
@@ -1,11 +1,12 @@
 <?php
  $connect = mysqli_connect('localhost', 'team-h', 'Dnjswndbf3.14', 'DB_BOARD');
      $bno = $_GET['number'];  
+$date = date('Y-m-d H:i:s'); 
      session_start();
    
     
     if($bno && $_POST['dat_user'] && $userpw && $_POST['content']){
-        $sql = query("insert into comment(number,id,content, date ,pw) values('".$bno."','".$_POST['dat_user']."','".$_POST['content']."',now() '".$userpw."')");
+        $sql = query("insert into comment(number,id,content, date ,pw) values('".$bno."','".$_POST['dat_user']."','".$_POST['content']."','".$date."', '".$userpw."')");
         echo "<script>alert('댓글이 작성되었습니다.'); 
         location.href='/read.php?idx=$bno';</script>";
     }else{
