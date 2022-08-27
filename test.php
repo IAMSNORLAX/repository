6afe60f5d39497f6d31a2b5b418a33f197144b87
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-27 19:07:50 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,17 +1,30 @@
-    <div class="dap_ins">
-        <form action="reply_ok.php?board_id=<?echo $board_id;?>&idx=<?php echo $bno; ?>" method="post">
-            <input type="hidden" name="dat_user" id="dat_user" class="dat_user" size="15" placeholder="아이디" value=<?$_SESSION['userid']?>>
-            <div style="margin-top:10px; ">
-                <textarea name="content" class="reply_content" id="re_content" ></textarea>
-                <button id="rep_bt" class="re_bt">댓글</button>
-            </div>
-        </form>
-    </div>
+<?php
+	 $connect = mysqli_connect('localhost', 'team-h', 'Dnjswndbf3.14', 'DB_BOARD');
+     $number = $_GET['number'];  
+     session_start();
+?>
+<!doctype html>
+<head>
+<meta charset="UTF-8">
+<title>게시판</title>
 
-div class="reply_view">
+</head>
+<body>
+	<?php
+		$bno = $_GET['number']; /* bno함수에 idx값을 받아와 넣음*/
+		$hit = mysqli_fetch_array(query("select * from board where number =".$bno));
+		$hit = $hit['hit'] + 1;
+		$fet = query("update board set hit = '".$hit."' where number = '".$bno."'");
+		$sql = query("select * from board where number=".$bno); /* 받아온 idx값을 선택 */
+		$board = $sql->fetch_array();
+	?>
+
+
+<!--- 댓글 불러오기 -->
+<div class="reply_view">
 	<h3>댓글목록</h3>
 		<?php
-			$sql3 = mq("select * from reply where con_num='".$bno."' order by idx desc");
+			$sql3 = query("select * from reply where con_num='".$bno."' order by idx desc");
 			while($reply = $sql3->fetch_array()){ 
 		?>
 		<div class="dap_lo">
@@ -24,10 +37,36 @@ div class="reply_view">
 			</div>
 			<!-- 댓글 수정 폼 dialog -->
 			<div class="dat_edit">
-				<form method="post" action="rep_modify_ok.php">
+				<form method="post" action="/BBS/rep_modify_ok.php">
 					<input type="hidden" name="rno" value="<?php echo $reply['idx']; ?>" /><input type="hidden" name="b_no" value="<?php echo $bno; ?>">
 					<input type="password" name="pw" class="dap_sm" placeholder="비밀번호" />
 					<textarea name="content" class="dap_edit_t"><?php echo $reply['content']; ?></textarea>
 					<input type="submit" value="수정하기" class="re_mo_bt">
 				</form>
 			</div>
+			<!-- 댓글 삭제 비밀번호 확인 -->
+			<div class='dat_delete'>
+				<form action="/BBS/reply_delete.php" method="post">
+					<input type="hidden" name="rno" value="<?php echo $reply['idx']; ?>" /><input type="hidden" name="b_no" value="<?php echo $bno; ?>">
+			 		<p>비밀번호<input type="password" name="pw" /> <input type="submit" value="확인"></p>
+				 </form>
+			</div>
+		</div>
+	<?php } ?>
+
+	<!--- 댓글 입력 폼 -->
+	<div class="dap_ins">
+		<form action="/BBS/reply_ok.php?idx=<?php echo $bno; ?>" method="post">
+			<input type="text" name="dat_user" id="dat_user" class="dat_user" size="15" placeholder="아이디">
+			<input type="password" name="dat_pw" id="dat_pw" class="dat_pw" size="15" placeholder="비밀번호">
+			<div style="margin-top:10px; ">
+				<textarea name="content" class="reply_content" id="re_content" ></textarea>
+				<button id="rep_bt" class="re_bt">댓글</button>
+			</div>
+		</form>
+	</div>
+</div>
+<div id="foot_box"></div>
+</div>
+</body>
+</html>
