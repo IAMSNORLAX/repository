d789818ad146c6ba7b4fdb940dda63ecd7be3582
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-27 18:23:46 +0900

    Update test.php

--- test.php
+++ test.php
@@ -7,3 +7,27 @@
             </div>
         </form>
     </div>
+
+div class="reply_view">
+	<h3>댓글목록</h3>
+		<?php
+			$sql3 = mq("select * from reply where con_num='".$bno."' order by idx desc");
+			while($reply = $sql3->fetch_array()){ 
+		?>
+		<div class="dap_lo">
+			<div><b><?php echo $reply['name'];?></b></div>
+			<div class="dap_to comt_edit"><?php echo nl2br("$reply[content]"); ?></div>
+			<div class="rep_me dap_to"><?php echo $reply['date']; ?></div>
+			<div class="rep_me rep_menu">
+				<a class="dat_edit_bt" href="#">수정</a>
+				<a class="dat_delete_bt" href="#">삭제</a>
+			</div>
+			<!-- 댓글 수정 폼 dialog -->
+			<div class="dat_edit">
+				<form method="post" action="rep_modify_ok.php">
+					<input type="hidden" name="rno" value="<?php echo $reply['idx']; ?>" /><input type="hidden" name="b_no" value="<?php echo $bno; ?>">
+					<input type="password" name="pw" class="dap_sm" placeholder="비밀번호" />
+					<textarea name="content" class="dap_edit_t"><?php echo $reply['content']; ?></textarea>
+					<input type="submit" value="수정하기" class="re_mo_bt">
+				</form>
+			</div>
