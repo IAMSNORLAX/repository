b66461bdc33de56666f1cd63557020cb038e3039
IAMSNORLAX <108931868+IAMSNORLAX@users.noreply.github.com>
2022-08-30 00:12:01 +0900

    Update write_action.php

--- write_action.php
+++ write_action.php
@@ -5,8 +5,8 @@
 $connect = mysqli_connect("localhost", "team-h", "Dnjswndbf3.14", "DB_BOARD") or die("fail");
 
                  
-$title = htmlentites($_POST['title']);               
-$content = htmlentites($_POST['content']);           
+$title = htmlentities($_POST['title']);               
+$content = htmlentities($_POST['content']);           
 $date = date('Y-m-d H:i:s');   
 
 
